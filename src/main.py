8750ce298e100c92034a19d01a4e774b383cb2b7
sashaweiss <sasha.weiss@u.northwestern.edu>
2017-03-10 09:23:32 -0600

    gmm and knn work with chunking

--- src/main.py
+++ src/main.py
@@ -2,9 +2,13 @@ from experiments import *
 
 if __name__ == "__main__":
 	# experiment1()
+	print "Experiment 2:"
 	experiment2()
 	# experiment3()
 	# experiment4()
-	# experiment5()
-	# experiment6()
-	# experiment7()
+	print "Experiment 5:"
+	experiment5()
+	print "Experiment 6:"
+	experiment6()
+	print "Experiment 7:"
+	experiment7()
