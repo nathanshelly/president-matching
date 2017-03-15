c75175cec9a2383ca58327e9f5ec4c1e6ab9bd16
sashaweiss <sasha.weiss@u.northwestern.edu>
2017-03-15 13:34:31 -0500

    experiment 8

--- src/main.py
+++ src/main.py
@@ -2,13 +2,21 @@ from experiments import *
 
 if __name__ == "__main__":
 	# experiment1()
-	print "Experiment 2:"
-	experiment2()
+	
+	# print "Experiment 2:"
+	# experiment2()
+	
 	# experiment3()
 	# experiment4()
-	print "Experiment 5:"
-	experiment5()
-	print "Experiment 6:"
-	experiment6()
-	print "Experiment 7:"
-	experiment7()
+	
+	# print "Experiment 5:"
+	# experiment5()
+	
+	# print "Experiment 6:"
+	# experiment6()
+	
+	# print "Experiment 7:"
+	# experiment7()
+
+	print "Experiment 8:"
+	experiment8()
