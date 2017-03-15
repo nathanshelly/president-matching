43a7e2953acdabda5e35ddeafca14e9ce5e9ee22
Nathan Shelly <nsnshelly@gmail.com>
2017-03-15 16:18:01 -0500

    should be ready to stream classifications

--- web_code/server.py
+++ web_code/server.py
@@ -11,31 +11,33 @@ app = Flask(__name__)
 ws = GeventWebSocket(app)
 
 @ws.route("/websocket")
-def audio(ws):
+def handle_requests(ws):
     while ws.connected:
         msg = ws.receive()
         if msg:
             msg = utilities.convert(json.loads(msg))
             if msg['type'] == 'recording':
-                audio = []
-                recording_msg = msg
-                print 'ayy recording started'
-                while recording_msg['type'] == 'recording':
-                    print msg['text']
-                    data = [msg['data'][str(x)] for x in range(len(msg['data']))]
-                    audio += data
-                    
-                    recording_msg = ws.receive()
-                    # print recording_msg
+                record_audio(ws)
 
-                    while not recording_msg:
-                        recording_msg = ws.receive()
 
-                    # print recording_msg
-                    recording_msg = utilities.convert(json.loads(recording_msg))
-                
-                print 'ayy recording finished'
-                ws.send(classify(audio))
+def record_audio(ws, ):
+    audio = []
+    recording_msg = msg
+    print 'ayy recording started'
+    while recording_msg['type'] == 'recording':
+        print msg['text']
+        data = [msg['data'][str(x)] for x in range(len(msg['data']))]
+        audio += data
+
+        recording_msg = ws.receive()
+        while not recording_msg:
+            recording_msg = ws.receive()
+
+        recording_msg = utilities.convert(json.loads(recording_msg))
+
+    print 'ayy recording finished'
+    ws.send('hey')
+    # ws.send(classify(audio))
 
 @app.route("/")
 def index():
@@ -46,18 +48,18 @@ def classify(signal, classifier_type = 'gmm'):
     mfccs = mfcc_features(signal)
     return test_sample_gmms(gmm_dict, mfccs)
 
-
-def streaming_classify(signal, current_probabilities, classifier_type = 'gmm'):
-    pass
+def streaming_classify(signal, current_probabilities, classifiers_path = 'nathan_sasha_pardo_gmm_dict.p', classifier_type = 'gmm'):
     # signal is buffer of 4096 data points
+    signal = streaming_mfcc_features(signal)
+    probabilities = streaming_test_sample_gmms(utilities.load(classifiers_path), signal)
+    
     # if not current_probabilities:
-    #     current_probabilities
-
-    # signal = streaming_mfcc_features(signal)
-
-    # updated_dictionary = {key: x.get(key, 0) + y.get(key, 0) for key in set(x) & set(y)}
-
-
-
+    #     current_probabilities = {label: 0 for label in probabilities.keys()}
+    # updated_dictionary = {key: current_probabilities.get(key, 0) + probabilities.get(key, 0) for key in set(current_probabilities) & set(probabilities)}
+    
+    # .get should handle empty current_probabilities
+    updated_probabilites = {key: probabilities.get(key, 0) + current_probabilities.get(key, 0) for key in probabilities}
+    return updated_probabilites
+    
 if __name__ == "__main__":
     app.run(debug = True)
