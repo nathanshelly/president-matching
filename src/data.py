43d5d3390cd962aa76290a202a4ce246842c93c1
Nathan Shelly <nathanshelly2018@u.northwestern.edu>
2017-04-04 06:28:21 +0000

    script for moving split_professors into training and testing folders

--- src/data.py
+++ src/data.py
@@ -4,7 +4,7 @@ from gmm import train_gmm_set
 import utilities
 
 import soundfile as sf
-import os
+import os, shutil
 import numpy as np
 
 def chunk_audio(signal, chunk_size):
@@ -28,8 +28,10 @@ def split_and_save(src, destdir, chunk_length=3):
 
     n_chunks = sr * chunk_length
 
-    little_chunk = signal[:n_chunks]
-    big_chunk = signal[n_chunks:]
+    # little_chunk = signal[:n_chunks]
+    # big_chunk = signal[n_chunks:]
+    little_chunk = signal[-n_chunks:]
+    big_chunk = signal[:-n_chunks]
 
     sf.write(os.path.join(destdir, "big_%s" % basename), big_chunk, sr)
     sf.write(os.path.join(destdir, "little_%s" % basename), little_chunk, sr)
@@ -60,15 +62,28 @@ def save_professor_gmms(srcdir, dest):
 
 def normalize_professors():
     for name in ["aravindan", "cossairt", "dinda", "fabian", "fatemah",
-                "goce", "ian", "ilya","jason", "jennie", "larry", "lincoln",
+                "goce", "ian", "ilya","jason", "jennie", "larry", 
                 "nathan","nell", "pardo", "robby", "russ", "sara", "sasha", "tov"]:
         normalize_and_save('data/professors/%s/%s.wav' % (name, name), 'data/professors_normalized/%s.wav' % name)
 
 def split_professors():
     for name in ["aravindan", "cossairt", "dinda", "downey", "fabian", "fatemah",
-                "goce", "ian", "ilya","jason", "jennie", "larry", "lincoln",
+                "goce", "ian", "ilya","jason", "jennie", "larry",
                 "nathan","nell", "pardo", "robby", "russ", "sara", "sasha", "tov"]:
         split_and_save('data/professors/%s/%s.wav' % (name, name), 'data/professors_split/%s' % name)
+
+def create_train_and_test_dirs(src_dir):
+	new_dirs = [os.path.join(src_dir, 'train'), os.path.join(src_dir, 'test')]
+	for dir_name in new_dirs:
+		if not os.path.exists(dir_name):
+			os.mkdir(dir_name)
+	for name in [name for name in os.listdir(src_dir) if name not in ('test', 'train')]:
+		name_path = os.path.join(src_dir, name)
+		train_path = os.path.join(src_dir, 'train')
+		test_path = os.path.join(src_dir, 'test')
+
+		shutil.copy2(os.path.join(name_path, 'big_' + name + '.wav'), train_path)
+		shutil.copy2(os.path.join(name_path, 'little_' + name + '.wav'), test_path)
 
 def save_professor_knn_features(srcdir, dest):
     train_data, train_labels = knn_train_features(srcdir)
@@ -76,6 +91,7 @@ def save_professor_knn_features(srcdir, dest):
     utilities.save((train_data, train_labels), dest)
 
 if __name__ == "__main__":
-    save_professor_gmms('data/professors_split/train', 'professor_gmms_train.p')
+    # save_professor_gmms('data/professors_split/train', 'professor_gmms_train.p')
     # save_professor_knn_features('data/professors_split/train', 'professor_knn_features_train.p')
     # split_professors()
+	create_train_and_test_dirs('data/professors_split')
